16ccbe1eb20f3b040321476fbae0145d04ea8b41
Mateusz <mgozdzik@duck.com>
2023-04-05 22:48:58 +0200

    update gravatars

--- accounts/views.py
+++ accounts/views.py
@@ -4,6 +4,8 @@ from .forms import CustomUserCreationForm
 from django.contrib.auth.decorators import login_required
 from django.core.mail import send_mail
 from os import getenv
+import random
+import hashlib
 
 
 def register_view(request):
@@ -11,13 +13,16 @@ def register_view(request):
         form = CustomUserCreationForm(request.POST)
         if form.is_valid():
             user = form.save()
-            send_mail(
-                subject="Register in My App",
-                message=f"Thanks for registering {user.username.title()}. Hope you'll have fun.",
-                from_email=getenv("EMAIL"),
-                recipient_list=[user.email],
-            )
-            return redirect("/accounts/login")
+            hash = hashlib.md5(
+                str(random.getrandbits(128)).encode('utf-8')).hexdigest()
+            user.gravatar_link = f"https://www.gravatar.com/avatar/{hash}?d=identicon"
+        send_mail(
+            subject="Register in My App",
+            message=f"Thanks for registering {user.username.title()}. Hope you'll have fun.",
+            from_email=getenv("EMAIL"),
+            recipient_list=[user.email],
+        )
+        return redirect("/accounts/login")
     else:
         form = CustomUserCreationForm()
     return render(request, "accounts/register.html", {"form": form})
