5a73415f8b8f4d73a74c4597df59804fe9e057ca
Mateusz <mgozdzik@duck.com>
2023-04-05 21:20:58 +0200

    update csrd trusted origins

--- Portfolio_App/settings.py
+++ Portfolio_App/settings.py
@@ -31,6 +31,10 @@ DEBUG = True
 
 ALLOWED_HOSTS = ["*"]
 
+CSRF_TRUSTED_ORIGINS = [
+    "https://web-development-9388.up.railway.app", "https://web-production-7633.up.railway.app", "https://*.127.0.0.1"
+]
+
 
 # Application definition
 
@@ -45,7 +49,6 @@ INSTALLED_APPS = [
     "core",
     "pushups",
     "szymon",
-    
 ]
 
 MIDDLEWARE = [
