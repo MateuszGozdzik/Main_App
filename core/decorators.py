1968264b1cd634ab892c91d42db02e2d52f4028e
Mateusz <mgozdzik@duck.com>
2023-04-06 18:26:21 +0200

    add error popup to decorator

--- core/decorators.py
+++ core/decorators.py
@@ -1,12 +1,14 @@
-from django.shortcuts import redirect
-from django.urls import reverse
+from django.shortcuts import render
 
 
 def group_required(group_name):
     def decorator(view_func):
         def wrapper(request, *args, **kwargs):
             if not request.user.groups.filter(name=group_name).exists():
-                return redirect(reverse('core:index'))
+                error = f"You need to be in \"{group_name}\" group to acces this page."
+                return render(request, "core/index.html", {
+                    "error": error,
+                })
             return view_func(request, *args, **kwargs)
         return wrapper
     return decorator
