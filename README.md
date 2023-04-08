791e2d3217c73899a59f08809997308304e5366c
Mateusz <mgozdzik@duck.com>
2023-04-08 11:59:18 +0200

    add todo and change User Model

--- README.md
+++ README.md
@@ -1,3 +1,11 @@
 # Portfolio_App
 
 [Visit](https://web-production-7633.up.railway.app/)
+
+
+# Todo:
+1. Add Users tab
+2. Maybe Friends
+3. User can be public or private, 
+4. Chat with user 
+5. optionals emails about messages
--- accounts/models.py
+++ accounts/models.py
@@ -6,7 +6,7 @@ class CustomUser(AbstractUser):
 
     gravatar_link = models.URLField(null=True)
     quote_newsletter = models.BooleanField(default=False)
-    simon_newsletter = models.BooleanField(default=False)
+    public = models.BooleanField(default=False)
     
     def __str__(self):
         return self.username
